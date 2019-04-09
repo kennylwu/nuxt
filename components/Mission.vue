8c3c7741a86827a50baf932f78d03ca1589d3b4c
Kenny Wu <user2279779@yahoo.com>
2019-04-08 21:24:16 -0700

    first commit

---
+++ components/Mission.vue
@@ -0,0 +1,39 @@
+<template>
+  <nuxt-link :to="'/mission/' + id ">
+    <article class="mission">
+      <div class="thumbnail" :style="{backgroundImage: 'url(' + thumbnail + ')'}"></div>
+      <h1>{{ title }}</h1>
+      <p>{{ previewText }}</p>
+    </article>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: ["id", "thumbnail", "title", "previewText"]
+};
+</script>
+
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: black;
+}
+.mission {
+  box-sizing: border-box;
+  width: auto;
+  padding: 10px;
+  border: 1px solid slategray;
+  box-shadow: 4px;
+  margin: 10px;
+}
+
+.thumbnail {
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 200px;
+}
+</style>
+
---
+++ pages/mission/_id/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="single-mission">
+    <h1>{{ missions.title }}</h1>
+    <div :id="missions.id">
+      <img :src="missions.thumbnail" alt="space">
+    </div>
+    <p>{{ missions.descriptionText}}</p>
+  </section>
+</template>
+
+<style scoped>
+.single-mission {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 20px;
+}
+</style>
+
+
+// <script>
+// export default {
+//   asyncData() {
+//     //return axioxs call
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve({
+//           missions: [
+//             {
+//               id: "1",
+//               descriptionText: "axios description",
+//               title: "here is title from Monday",
+//               thumbnail:
+//                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbFMono9uVXQnVWSbtUvrKLyIga16XXBZAo-iBGN_8OMwZRWlVRQ"
+//             }
+//           ].find(el => el.id === context.params.id)
+//         });
+//       }, 1000);
+//     });
+//   }
+// };
+//
+</script>
