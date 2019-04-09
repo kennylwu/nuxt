5328cdddeafb322d76877b2e788fe53fedfe17aa
Kenny Wu <user2279779@yahoo.com>
2019-04-09 00:14:33 -0700

    routing to different page

--- pages/mission/_id/index.vue
+++ pages/mission/_id/index.vue
@@ -1,9 +1,11 @@
 <template>
   <section class="single-mission">
+    <h1>Page: {{$route.params.id}}</h1>
     <h1>{{ missions.title }}</h1>
     <div :id="missions.id">
       <img :src="missions.thumbnail" alt="space">
     </div>
+
     <p>{{ missions.descriptionText}}</p>
   </section>
 </template>
@@ -18,27 +20,40 @@
 }
 </style>
 
-
-// <script>
-// export default {
-//   asyncData() {
-//     //return axioxs call
-//     return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         resolve({
-//           missions: [
-//             {
-//               id: "1",
-//               descriptionText: "axios description",
-//               title: "here is title from Monday",
-//               thumbnail:
-//                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbFMono9uVXQnVWSbtUvrKLyIga16XXBZAo-iBGN_8OMwZRWlVRQ"
-//             }
-//           ].find(el => el.id === context.params.id)
-//         });
-//       }, 1000);
-//     });
-//   }
-// };
-//
+<script>
+export default {
+  asyncData(context) {
+    //return axioxs call
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          missions: [
+            {
+              id: "1",
+              descriptionText: "successful routing",
+              title: "here is title from service call Tuesday",
+              thumbnail:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbFMono9uVXQnVWSbtUvrKLyIga16XXBZAo-iBGN_8OMwZRWlVRQ"
+            },
+            {
+              id: "2",
+              descriptionText: "successful routing",
+              title: "here is title from service call Tuesday",
+              thumbnail:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbFMono9uVXQnVWSbtUvrKLyIga16XXBZAo-iBGN_8OMwZRWlVRQ"
+            },
+            {
+              id: "3",
+              descriptionText: "successful routing",
+              title: "here is title from service call Tuesday",
+              thumbnail:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbFMono9uVXQnVWSbtUvrKLyIga16XXBZAo-iBGN_8OMwZRWlVRQ"
+            }
+          ].find(el => el.id === context.params.id)
+        });
+      }, 1000);
+    });
+  }
+};
 </script>
+
